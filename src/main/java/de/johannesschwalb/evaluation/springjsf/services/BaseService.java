5e3e8513db255471e42db9ba9c2ad10a69167959
jschwalb <johannes.schwalb@gmail.com>
2016-02-28 14:22:15 +0100

    @Named => @Service Annotation for SpringServices.

--- src/main/java/de/johannesschwalb/evaluation/springjsf/services/BaseService.java
+++ src/main/java/de/johannesschwalb/evaluation/springjsf/services/BaseService.java
@@ -1,12 +1,14 @@
 package de.johannesschwalb.evaluation.springjsf.services;
 
+import org.springframework.stereotype.Service;
+
 import javax.inject.Named;
 import java.util.Random;
 
 /**
  * Created by LG P310 on 27.02.2016.
  */
-@Named
+@Service
 public class BaseService {
 
     public String getRandomNumber() {
--- src/main/java/de/johannesschwalb/evaluation/springjsf/services/SpringService.java
+++ src/main/java/de/johannesschwalb/evaluation/springjsf/services/SpringService.java
@@ -1,12 +1,14 @@
 package de.johannesschwalb.evaluation.springjsf.services;
 
+import org.springframework.stereotype.Service;
+
 import javax.inject.Inject;
 import javax.inject.Named;
 
 /**
  * Created by LG P310 on 27.02.2016.
  */
-@Named
+@Service
 public class SpringService {
 
     @Inject
